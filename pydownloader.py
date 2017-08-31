d4f77814e91c7665236179762021554cc4d0265c
Alexander <jiranisensei@gmail.com>
2017-08-31 11:38:46 +0900

    First commit

---
+++ pydownloader.py
@@ -0,0 +1,50 @@
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import sys
+import urllib.request
+
+
+class Downloader(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+
+        layout = QVBoxLayout()
+
+        self.url = QLineEdit()
+        self.save_location = QLineEdit()
+        progress = QProgressBar()
+        download = QPushButton()
+
+        self.url.setPlaceholderText("URL")
+        self.save_location.setPlaceholderText("File save location")
+
+        self.progress.setValue(0)
+        self.progress.setAlignment(Qt.AlignCenter)
+
+        layout.addWidget(self.url)
+        layout.addWidget(self.save_location)
+        layout.addWidget(self.progress)
+        layout.addWidget(download)
+
+        self.setLayout(layout)
+        self.setWindowTitle("PyDownloader")
+        self.setFocus()
+
+        download.clicked.connect(self.download)
+
+    def download(self):
+        url = self.url.text()
+        save_location = self.save_location.text()
+        urllib.request.url.urlretrieve(url, save_location, self.report)
+
+    def report(self, blocknum, blocksize, totalsize):
+        readsofar = blocknum * blocksize
+        if totalsize > 0:
+            percent = readsofar * 100 / totalsize
+            self.progress.setValue(int(percent))
+
+
+app = QApplication(sys.argv)
+dl = Downloader()
+dl.show()
+app.exec_()
